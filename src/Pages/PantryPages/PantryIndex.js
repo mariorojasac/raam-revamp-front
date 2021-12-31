c4d78f4f4a3bf003202de47398eed8c71b5b76bb
vernellmiller <nezeey@gmail.com>
2021-12-31 15:36:20 -0500

    wip: updates state and adds newState function

--- src/Pages/PantryPages/PantryIndex.js
+++ src/Pages/PantryPages/PantryIndex.js
@@ -5,8 +5,17 @@ import { useState } from 'react-router-dom';
 import React from 'react'
 
 const PantryIndex = () => {
-    const [ pantries, setPantries ] = useState(null);
+    const [newForm, setNewForm] = useState(getNewState());
     const PANTRY_BASE_URL = "https://raam-test-api-2.herokuapp.com";
+
+    function getNewState() {
+        return {
+            name: "",
+            image: "",
+            location: "",
+            description: ""
+        }
+    }
 
     return (
         <div>
