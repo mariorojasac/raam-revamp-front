2791cd44f7a72b5898f2e90b34958c0e905ee3ba
vernellmiller <nezeey@gmail.com>
2021-12-31 15:33:42 -0500

    wips: sets state and adds base url

--- src/Pages/PantryPages/PantryIndex.js
+++ src/Pages/PantryPages/PantryIndex.js
@@ -5,6 +5,9 @@ import { useState } from 'react-router-dom';
 import React from 'react'
 
 const PantryIndex = () => {
+    const [ pantries, setPantries ] = useState(null);
+    const PANTRY_BASE_URL = "https://raam-test-api-2.herokuapp.com";
+
     return (
         <div>
             
