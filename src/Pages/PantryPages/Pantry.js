7f2a950efcde0ee89b0f8f9d0f0201211651718e
vernellmiller <nezeey@gmail.com>
2021-12-31 14:57:04 -0500

    adds helper functions and useEffect

--- src/Pages/PantryPages/Pantry.js
+++ src/Pages/PantryPages/Pantry.js
@@ -7,6 +7,28 @@ import React from 'react'
 const Pantry = (props) => {
     const [ pantries, setPantries ] = useState(null);
     const PANTRY_BASE_URL = "https://raam-test-api-2.herokuapp.com";
+
+    const getPantries = async () => {
+        // const data = await fetch(PANTRY_BASE_URL + "/pantry").then(Response => Response.json());
+        const Response = await fetch(PANTRY_BASE_URL + "/pantry");
+        const data = await Response.json();
+        setPantries(data);
+    };
+
+    const createPantries = async (pantry) => {
+        await fetch(PANTRY_BASE_URL + "/pantry", {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(pantry)
+        });
+        getPantries();
+    };
+
+    useEffect(() => getPantries(), []);
+
+
     return (
         <div>
             
