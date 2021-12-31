a2bfdcf54e34c6868e33ecb4f30ba44d1da27d26
vernellmiller <nezeey@gmail.com>
2021-12-31 14:42:37 -0500

    wip:add imports, boilerplate pantryIndex component

---
+++ src/Pages/PantryPages/PantryIndex.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react-router-dom';
+
+import React from 'react'
+
+const PantryIndex = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PantryIndex;
