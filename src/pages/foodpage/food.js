6757f5127e7051aaf020a0586e20ef2884580a0f
“coreyalvarez” <coreyalvarez1989@gmail.com>
2022-01-01 14:26:36 -0600

    wip: adds food page

---
+++ src/pages/foodpage/food.js
@@ -0,0 +1,73 @@
+import { Route, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import FoodIndex from '../foodpage/foodIndex';
+import FoodShow from '../foodpage/foodShow';
+
+
+const Food = (props) => {
+    const [foods, setFoods] = useState(null);
+
+    const FOOD_BASE_URL = 'https://raam-test-api-2.herokuapp.com/food/';
+
+    const getFood = async () => {
+        const data = await fetch(FOOD_BASE_URL).then(response => response.json());
+        setFoods(data);
+    }
+    
+    const createFood = async (food) => {
+        await fetch(FOOD_BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'Application/json'
+            },
+            body: JSON.stringify(food)
+        });
+
+        getFood();
+    }
+
+    const updateFood = async (food, id) => {
+        await fetch(FOOD_BASE_URL + id, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'Application/json'
+            },
+            body: JSON.stringify(food)
+        });
+        getFood();
+    }
+
+    const deleteFood = async id => {
+        await fetch(FOOD_BASE_URL + id, {method: 'DELETE'});
+        getFood();
+    }
+
+    useEffect(() => getFood(), [])
+
+    
+    return (
+        <main>
+            <Switch>
+                <Route exact path='/food'>
+                    <FoodIndex foods={foods} createFood={createFood}/>
+                </Route>
+                <Route 
+                    path='/food/:id' 
+                    render={(rp) => (
+                        <FoodShow 
+                        {...rp}
+                        foods={foods}
+                        updateFood={updateFood}
+                        deleteFood={deleteFood}
+                    />
+                )}
+                
+                />
+                
+
+            </Switch>
+        </main>
+    )
+};
+
+export default Food;
