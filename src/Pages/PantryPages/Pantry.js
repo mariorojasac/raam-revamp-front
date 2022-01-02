d07ee274bea4c53209801685e550ab0866abb1c7
vernellmiller <nezeey@gmail.com>
2022-01-02 11:49:11 -0500

    adds empty array to useState

--- src/Pages/PantryPages/Pantry.js
+++ src/Pages/PantryPages/Pantry.js
@@ -5,7 +5,7 @@ import PantryIndex from './PantryIndex';
 import React from 'react'
 
 const Pantry = (props) => {
-    const [ pantries, setPantries ] = useState(null);
+    const [ pantries, setPantries ] = useState([]);
     const PANTRY_BASE_URL = "https://raam-test-api-2.herokuapp.com";
 
     const getPantries = async () => {
