63f694f192147af043f032b47dc80c5451641cfc
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2022-01-02 16:24:10 -0500

    adding changes

--- src/pages/foodpage/food.js
+++ src/pages/foodpage/food.js
@@ -1,73 +1,68 @@
-import { Route, Switch } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import FoodIndex from './FoodIndex';
-import FoodShow from './FoodShow';
-Food
+import { Route, Switch } from "react-router-dom";
+import { useEffect, useState } from "react";
+import FoodIndex from "./FoodIndex";
+import FoodShow from "./FoodShow";
 
 const Food = (props) => {
-    const [foods, setFoods] = useState(null);
+  const [foods, setFoods] = useState(null);
 
-    const FOOD_BASE_URL = 'https://raam-test-api-2.herokuapp.com/food/';
+  const FOOD_BASE_URL = "https://raam-test-api-2.herokuapp.com/food/";
 
-    const getFood = async () => {
-        const data = await fetch(FOOD_BASE_URL).then(response => response.json());
-        setFoods(data);
-    }
-    
-    const createFood = async (food) => {
-        await fetch(FOOD_BASE_URL, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'Application/json'
-            },
-            body: JSON.stringify(food)
-        });
+  const getFood = async () => {
+    const data = await fetch(FOOD_BASE_URL).then((response) => response.json());
+    setFoods(data);
+  };
 
-        getFood();
-    }
+  const createFood = async (food) => {
+    await fetch(FOOD_BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json",
+      },
+      body: JSON.stringify(food),
+    });
 
-    const updateFood = async (food, id) => {
-        await fetch(FOOD_BASE_URL + id, {
-            method: 'PUT',
-            headers: {
-                'Content-type': 'Application/json'
-            },
-            body: JSON.stringify(food)
-        });
-        getFood();
-    }
+    getFood();
+  };
 
-    const deleteFood = async id => {
-        await fetch(FOOD_BASE_URL + id, {method: 'DELETE'});
-        getFood();
-    }
+  const updateFood = async (food, id) => {
+    await fetch(FOOD_BASE_URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-type": "Application/json",
+      },
+      body: JSON.stringify(food),
+    });
+    getFood();
+  };
 
-    useEffect(() => getFood(), [])
+  const deleteFood = async (id) => {
+    await fetch(FOOD_BASE_URL + id, { method: "DELETE" });
+    getFood();
+  };
 
-    
-    return (
-        <main>
-            <Switch>
-                <Route exact path='/food'>
-                    <FoodIndex foods={foods} createFood={createFood}/>
-                </Route>
-                <Route 
-                    path='/food/:id' 
-                    render={(rp) => (
-                        <FoodShow 
-                        {...rp}
-                        foods={foods}
-                        updateFood={updateFood}
-                        deleteFood={deleteFood}
-                    />
-                )}
-                
-                />
-                
+  useEffect(() => getFood(), []);
 
-            </Switch>
-        </main>
-    )
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/food">
+          <FoodIndex foods={foods} createFood={createFood} />
+        </Route>
+        <Route
+          path="/food/:id"
+          render={(rp) => (
+            <FoodShow
+              {...rp}
+              foods={foods}
+              updateFood={updateFood}
+              deleteFood={deleteFood}
+            />
+          )}
+        />
+      </Switch>
+    </main>
+  );
 };
 
 export default Food;
