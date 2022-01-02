08c31f59b6439f6821b144aa13dc4eac72bf2a9c
vernellmiller <nezeey@gmail.com>
2022-01-02 10:56:46 -0500

    wip: adds form to show page

--- src/Pages/PantryPages/PantryShow.js
+++ src/Pages/PantryPages/PantryShow.js
@@ -2,8 +2,43 @@ import React from 'react'
 
 const PantryShow = () => {
     return (
-        <div>
-            
+        <div className='pantry'>
+            <h1>{pantry?.name}</h1>
+            <img src={pantry?.image} alt={pantry?.name} />
+            <h2>{pantry?.location}</h2>
+            <h2>{pantry?.description}</h2>
+            <button id="delete" onClick={removePantry}>DELETE</button>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                value={editForm.name}
+                onChange={handleChange}
+                placeholder="name"
+                name="name"
+                />
+                <input 
+                type="text"
+                value={editForm.image}
+                onChange={handleChange}
+                placeholder="image URL"
+                name="image"
+                />
+                <input 
+                type="text"
+                value={editForm.location}
+                onChange={handleChange}
+                placeholder="location"
+                name="location"
+                />
+                <input 
+                type="text"
+                value={editForm.description}
+                onChange={handleChange}
+                placeholder="description"
+                name="description"
+                />
+                <input type="submit" value="Edit Pantry" />
+            </form>
         </div>
     )
 }
